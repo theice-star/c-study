ff1a90b9683549b7f0fbe47c24c8fc5c69c7b067
TheIce-Star <2138137189@qq.com>
2021-05-15 16:38:24 +0800

    update

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int number = 0;
+    printf("hello world\n");
+    // if number is 0,print "hello variables"
+    if (number == 0)
+    {
+        printf("hello varaibles\n");
+    }
+    getchar();
+    return 0;
+}
